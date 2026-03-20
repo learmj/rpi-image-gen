4fa3a71de832859902ce74b16e1fadd04e41ced6
Matthew Lear <matthew.lear@raspberrypi.com>
2026-03-20 11:18:45 +0000

    slot-mapper: Add dracut support
    Add the required hooks to the rpi-ab-slot-mapper layer so it will
    install the udev rules and slot helpers in an initramfs created by
    dracut. Add a new generic hook to generate all initramfs images with
    dracut if it's installed.
    
    The slot mapper layer should not dictate initramfs policy, so remove
    initramfs-tools from the packages it installs and instead, install the
    assets if a supported initramfs generator is detected in the chroot.
    
    Since dracut requires a valid root= on the cmdline, set it to the active
    system slot alias in the AB image. Since initramfs-tools injects its own
    root dev this is a nop for initramfs-tools generated initramfs images.
    
    If dracut is installed, this commit allows an AB image to boot
    successfully with a clear pmap.

---
+++ layer/rpi/device/slot-mapper/dracut/modules.d/90rpi-ab-root/module-setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+check() { return 0; }
+depends() { echo "udev-rules"; }
+install() {
+   inst_binary /usr/bin/od
+   inst_binary /usr/bin/sed
+   inst_binary /usr/bin/rpi-slot-label
+   inst_binary /usr/bin/rpi-slot-static
+   inst_simple /boot/slot.map /boot/slot.map 2>/dev/null || :
+   inst_rules /etc/udev/rules.d/99-rpi-slot.rules
+}
